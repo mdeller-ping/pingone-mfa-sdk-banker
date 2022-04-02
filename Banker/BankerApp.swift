7dce28f3518e4fbfa26ed44f7647149790a336c8
mdeller-ping <mdeller@pingidentity.com>
2022-04-02 10:26:19 -0400

    initial

---
+++ Banker/BankerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankerApp.swift
+//  Banker
+//
+//  Created by Michael Deller on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BankerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# pingone-mfa-sdk-banker
